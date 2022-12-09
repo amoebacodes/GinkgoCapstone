93cd2dc3f6af3a5a1d3d2683eadc0ac5ef0f52c7
amoebacodes <ywang381@jhu.edu>
2022-12-08 21:27:42 -0500

    see details in slack
    it's a working progress

--- package_draft/BeadFinder.py
+++ package_draft/BeadFinder.py
@@ -4,29 +4,45 @@ from DL_finder import DL_finder
 from helper import crop_rotate, isolate_each_well, index_to_letter
 import numpy as np
 
-
+"""
+BeadFinder implements a specified algorithm to identify beads from an image
+"""
 class BeadFinder():
-    def __init__(self, image_path, algorithm, plate_name, image_registration, show_heatmap):
+    def __init__(self, image_path, algorithm_name, plate_name, image_registration, show_heatmap, output_dir):
         self.image_path = image_path
-        self.algorithm = algorithm
+        self.algorithm_name = algorithm_name # algorithm_name name
         self.plate_name = plate_name
         self.image_registration = image_registration
         self.show_heatmap = show_heatmap
-        self.method = None
+        self.output_dir = output_dir
 
-    def get_method(self):
-        if self.algorithm == "melody's":
+        self._instantiate_algorithm()
+
+    """
+    Instantiate algorithm from algorithm name to prep for find_beads()
+    """
+    def _instantiate_algorithm(self):
+        if self.algorithm_name == "adaptive_thresholding":
             pass
-        if self.algorithm == "daniel's":
+        elif self.algorithm_name == "daniel's":
             pass
-        if self.algorithm == "deep_learning" or self.algorithm == "deep_learning_aug":
-            self.method = DL_finder
+        elif self.algorithm_name == "deep_learning" or self.algorithm_name == "deep_learning_aug":
+            self.algorithm = DL_finder
         else:
-            print("No such algorithm!")
+            raise ValueError("Invalid algorithm name!")
 
-    def set_method_to(self, method):
-        pass
+    def get_algorithm_name(self):
+        return self.algorithm_name
 
+    def set_algorithm_to(self, algorithm_name):
+        self.algorithm_name = algorithm_name
+        self._instantiate_algorithm()
+
+    """
+    output: a list of well ids (e.g. A1) that has beads detected
+            a list of well coordinates (0-indexed) with detected beads
+            total number of wells with beads
+    """
     def find_beads(self):
         # load image
         image_path = self.image_path
@@ -34,7 +50,7 @@ class BeadFinder():
         img = np.asarray(img)
         wells = isolate_each_well(img)  # a 16 x 24 numpy ndarray, each entry is the isolated well image
 
-        beads_ids, beads_coors, count = self.method(wells, self.algorithm)
+        beads_ids, beads_coors, count = self.algorithm(wells, self.algorithm_name)
 
         return beads_ids, beads_coors, count
 
--- package_draft/main.py
+++ package_draft/main.py
@@ -1,25 +1,26 @@
 from argparse import ArgumentParser
 from BeadFinder import BeadFinder
-
+import os
 """
 Demo argument:
 python main.py  --image_path '221019_122109_1043495.jpg' --algorithm 'deep_learning'
 """
 
-if __name__ == "__main__":
-    parser = ArgumentParser(description='meg.beads_detection')
-    parser.add_argument('--image_path', type=str, default='221019_122109_1043495.jpg')
-    parser.add_argument('--algorithm', type=str, default="deep_learning")  # options: ..., deep_learning, deep_learning_aug
-    parser.add_argument('--plate_name', type=str, default="my_plate")  # default = False
-    parser.add_argument('--image_registration', action='store_true')
-    parser.add_argument('--show_heatmap', action='store_true')
-    args = parser.parse_args()
-    print('\n'.join([f'{k}: {v}' for k, v in vars(args).items()]))
+def validate_paths(image_path, output_dir):
+    if not os.path.isfile(image_path):
+        raise FileNotFoundError('Image path is not a file.')
+    if not os.path.isdir(output_dir):
+        raise FileNotFoundError('Directory not found.')
 
-    instance = BeadFinder(args.image_path, args.algorithm, args.plate_name, args.image_registration, args.show_heatmap)
-    instance.get_method()
-    beads_ids, beads_coors, count = instance.find_beads()
+def validate(args):
+    # if show_heatmap is false, save heatmap, and thus output dir must be specified
+    if not args.show_heatmap and args.output_dir == None:
+        raise ValueError('When show_heatmap is set to false (default), user need to specify an output_dir\
+                for saving the heatmap of the plate. If you wish not to save the heatmap, you can set show_heatmap\
+                to true, and the heatmap will be a pop-up window.')
+    validate_paths(args.image_path, args.output_dir)
 
+def save_to_txt(beads_ids,beads_coors):
     # save a list of string well IDs that have mag.beads
     file_name_ids = "beads_ids_"+args.plate_name+".txt"
     with open(file_name_ids, "w") as output:
@@ -32,8 +33,32 @@ if __name__ == "__main__":
         output.write(str(beads_coors))
     print("saved well coords that have mag.beads to", file_name_coords, "!")
 
+if __name__ == "__main__":
+    # load in arguments from command line
+    parser = ArgumentParser(description='meg.beads_detection')
+    parser.add_argument('--image_path', type=str, default='221019_122109_1043495.jpg')
+    parser.add_argument('--algorithm_name', type=str, default="deep_learning")  # options: ..., deep_learning, deep_learning_aug
+    parser.add_argument('--plate_name', type=str, default="my_plate")  
+    parser.add_argument('--image_registration', action='store_false') # default = False
+    parser.add_argument('--show_heatmap', action='store_false') # default = False
+    parser.add_argument('--output_dir', type=str)
+    args = parser.parse_args()
+
+    # validate arguments
+    validate(args)
+    # show arguments
+    print('\n'.join([f'{k}: {v}' for k, v in vars(args).items()]))
+
+    #instantiate bead finder for locating beads
+    instance = BeadFinder(args.image_path, args.algorithm_name, args.plate_name, args.image_registration, args.show_heatmap, args.output_dir)
+    beads_ids, beads_coors, count = instance.find_beads()
+
+    # results
+    # save_to_txt(beads_ids, beads_coors) #uncomment if decide to save well id and coor to txt
+    print('Well_ids with beads: \n', beads_ids)
+    print('They are at the following coordinates: \n', beads_coors)
     # print stats
-    print(f"There are {count}/{100 * count // 384}% wells that have mag.beads")
+    print(f"There are {count}/384 or about {100 * count // 384}% wells that have mag.beads")
 
 
 """
